4ba711dd51b9ec68b63c6012628ca5bf51d17a86
stemlabo <regolith.mail@gmail.com>
2019-05-14 04:48:01 +0900

    監視プロパティ

---
+++ deep/js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		colors: [
+			{ name: 'Red'},
+			{ name: 'Green'},
+			{ name: 'Blue'}
+		]
+	},
+	watch: {
+		colors: {
+			handler: function(newValue, oldValue) {
+				console.log('Update!')
+			},
+			deep: true
+		}
+	}
+ })
