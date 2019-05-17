af8d7025bced8e6e738f1abfee1cc03839d0ec0f
stemlabo <regolith.mail@gmail.com>
2019-05-18 03:34:44 +0900

    リアルタイムサーチ3

--- realtime_search/js/main.js
+++ realtime_search/js/main.js
@@ -6,16 +6,25 @@ var app = new Vue({
         message: ''
     },
     watch: {
+        keyword: function(newKeyword, oldKeyword) {
+            // console.log(newKeyword)
+            this.message = 'Waiting for you to stop typing...'
+            this.debouncedGetAnswer()
+        }
+
 
     },
     created: function() {
-this.keyword = 'JavaScript'
-this.getAnswer()
+//this.keyword = 'JavaScript'
+//this.getAnswer()
+this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
     },
+
     methods: {
 getAnswer: function() {
     if(this.keyword === '') {
         this.items = null
+        this.message = ''
         return
     }
     this.message = 'Loading...'
